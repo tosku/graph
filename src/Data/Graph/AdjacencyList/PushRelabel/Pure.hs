28f8b17f7de0492bd902e8ca060ab2eab552d34b
Thodoris Papakonstantinou <mail@tpapak.com>
2018-09-26 11:18:11 +0200

    moved loom.md to module documentation

--- src/Data/Graph/AdjacencyList/PushRelabel/Pure.hs
+++ src/Data/Graph/AdjacencyList/PushRelabel/Pure.hs
@@ -7,6 +7,36 @@ Maintainer  : mail@tpapak.com
 Stability   : experimental
 Portability : POSIX
 
+
+= Loom - Push Pelabel
+The loom algorithm is a push relabel implementation for solving the 
+ [max flow problem](https://en.wikipedia.org/wiki/Push%E2%80%93relabel_maximum_flow_algorithm#Practical_implementations) 
+The algorithm is tested on directed graphs.
+
+=== Definitions
+A network \( N \) is defined by a directed graph \( G \) its source and sink \(s,t\) and the capacities \(C : E \rightarrow R^+ \) 
+Following push-relabel's terminology the residual graph \(R\) is a network containing both original edges of \(G\) (forward edges) and backward (reverse) edges, with the additional properties of __preflow__ \(F : E_R \rightarrow R^+ \) on the forward edges and residual capacities \(C_R\) on all edges of \(R\) as well as the following properties on the vertices:
+- Height \(H: V \rightarrow R^+\) which determines whether preflow can be pushed through an edge.
+- Excess \(X:  V \rightarrow R^+\) recording the excess flow of each vertex. 
+When the algorithm terminates all excess is \(0\) and the preflow of each edges is
+the actual maximum flow of \(N\).
+
+=== Operations
+The main difference in the definitions lies in the split of the PR push operation
+into two, depending on whether it is performed in a forward edge or backward
+edge. The former operation is called __push__ (from now on, /push/ will be used in
+this context) and the latter __pull__.
+Relabel is the usual PR adjusting of heights.
+
+/PR/ guaranties that there is a cut between the source and sink in the residual
+graph partitioning \(R\) into \(S\) and \(T\) vertices.
+
+The algorithm is iterative and each iteration consists of three steps
+
+1. global-relabel
+2. global-push
+3. global-pull
+
  -}
 
 {-# LANGUAGE BangPatterns #-}
@@ -26,7 +56,6 @@ import Data.Graph.AdjacencyList.PushRelabel.Internal
 import qualified Data.Graph.AdjacencyList.BFS as BFS
 
 -- | Implementation of the push relabel algorithm
--- works only on graphs with single direction
 pushRelabel :: Network -> Either String ResidualGraph
 pushRelabel net =
   let initg = initializeResidualGraph net
