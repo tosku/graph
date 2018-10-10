79485ff90e2e517e28428b11d5af7254ff97b8b0
Thodoris Papakonstantinou <mail@tpapak.com>
2018-10-10 16:31:50 +0200

    optimizationsin BFS module

--- src/Data/Graph/AdjacencyList/DFS.hs
+++ src/Data/Graph/AdjacencyList/DFS.hs
@@ -75,8 +75,8 @@ postordering = reverse . topsort
 type DAG = Graph
 
 -- | Ginen a DAG and a vertex you get the distances
-distances :: DAG  -> Vertex -> IM.IntMap Vertex
-distances g s =
+distances' :: DAG  -> Vertex -> IM.IntMap Vertex
+distances' g s =
   let topsorted = topsort $ dfs g s
       initdists = foldl' (\ac v -> IM.insert v 0 ac) IM.empty $ vertices g
    in foldl' (\ac v -> 
@@ -93,40 +93,60 @@ distances g s =
                   ) ac neis
       ) initdists topsorted
 
+type Distances = IM.IntMap Vertex
+
+-- | Ginen a DAG and a vertex you get the distances
+distances :: DAG  -> DFS -> Vertex -> Distances
+distances g dfs' s =
+  let topsorted = topsort $ dfs'
+      !initdists = foldl' (\ac v -> IM.insert v 0 ac) IM.empty $ vertices g
+   in foldl' (\ac v -> 
+        let neis = neighbors g v
+            distv = case IM.lookup v ac of
+                      Nothing -> 0
+                      Just d -> d
+         in foldl' (\dists' nei -> 
+           let neidist = case IM.lookup nei dists' of
+                           Nothing -> 0
+                           Just nd -> nd
+               newdist = max neidist (distv+1)
+            in IM.insert nei newdist dists'
+                  ) ac neis
+      ) initdists topsorted
+
+type TopologicalSorting = [Vertex]
 -- |checks if s is predecessor of t
-dependsOn :: DAG -> Vertex -> Vertex -> Bool
-dependsOn g t s =
-  let topsorted = topsort $ dfs g s
-   in elem t (snd (span ((==) s) topsorted))
+dependsOn :: TopologicalSorting -> Vertex -> Vertex -> Bool
+dependsOn topsorted t s = elem t (snd (span ((==) s) topsorted))
 
-areConnected :: Graph -> Vertex -> Vertex -> Bool
-areConnected g u v = 
-  let dists = distances g u
-   in (fromJust $ IM.lookup v dists) > 0 || v == u
-
-         
+areConnected :: Distances -> Vertex -> Vertex -> Bool
+areConnected dists u v = (fromJust $ IM.lookup v dists) > 0 || v == u
 
 -- |Longest path from tail to nose
 longestPath :: Graph -> Vertex -> Vertex -> [Edge]
 longestPath g s t =
-  if not $ dependsOn g t s
+  let dfs' = dfs g s
+      topsorted = topsort dfs'
+      dists = distances g dfs' s
+      revg = reverseGraph g
+      disconnected = filter (\n -> not (areConnected dists s n)) $ vertices g
+   in if not $ dependsOn topsorted t s
      then []
      else 
-        let !topsorted = topsort $ dfs g s
-            !dists = distances g s
-            !revg = reverseGraph g
-            disconnected = filter (\n -> not (areConnected g s n)) $ vertices g
-         in if not $ null disconnected
-              then
-                let cleangraph = filterVertices (\v -> not $ elem v disconnected) g
-                 in longestPath cleangraph s t
-              else
-                let path' :: Vertex -> [Edge] -> [Edge]
-                    path' v p 
-                      | v == s = p
-                      | otherwise = 
-                             let parents = neighbors revg v
-                              in if parents == [s]
+       if not $ null disconnected
+          then
+            let cleangraph = filterVertices (\v -> not $ elem v disconnected) g
+             in longestPath cleangraph s t
+          else
+            let path' :: Vertex -> [Edge] -> [Edge]
+                path' v p 
+                  | v == s = p
+                  | otherwise = 
+                         let parents = neighbors revg v
+                          in if null parents
+                                then []
+                                else
+                                  if parents == [s]
                                    then (Edge s v):p
                                    else 
                                      let pred :: Vertex
@@ -141,4 +161,4 @@ longestPath g s t =
                                                     else (prevmax,maxdist)
                                                     ) (0,0) parents
                                       in  path' pred $ (Edge pred v): p
-                 in path' t []
+             in path' t []
